8a78babd4153584dfff5b86a1eb8602829507aaf
Rajiv Kumar <rajiv2494@gmail.com>
2019-04-05 14:32:56 +0530

    updated beginning user prompt

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data! Please choose suitable options')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     x=input("please enter valid city:chicago,washington,new york city")
     while(x.lower() not in CITY_DATA):
